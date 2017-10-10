9d20fad3cbe5f28db656497e5a07af545de6f303
Collin Brittle <cnbrittle@gmail.com>
2017-10-10 06:23:54 +0000

    Update script to use pytest, plus various improvements.

--- fedora_spec_test.py
+++ fedora_spec_test.py
@@ -1,229 +1,263 @@
 import requests, pytest, re
 ROOT = 'http://127.0.0.1:8080/rest'
-NODES = set()
-FILES = {'file': open('./image.jpg', 'rb')}
+PAYLOAD_HEADERS = ['Content-Length', 'Content-Range', 'Trailer', 'Transfer-Encoding'] # Via RFC 7231 3.3
 
-def test_
+support_delete = pytest.mark.skipif('DELETE' not in requests.head(ROOT).headers['Allow'], reason='3.7 DELETE: (OPTIONAL) Delete method not supported')
+
+@pytest.fixture
+def nodes():
     # Ensure ROOT is accessible.
     r = requests.get(ROOT)
-    assert r.status_code == 200
-except AssertionError:
-    print('Fedora root not found. Is Fedora running at the ROOT url?')
+    assert r.status_code == 200, 'Fedora root not found. Is Fedora running at the ROOT url?'
+
+    nodes = set()
+    yield nodes
+
+    for node in nodes:
+        requests.delete(node)
+        requests.delete(node + '/fcr:tombstone')
+
+@pytest.fixture
+def files():
+    with open('./image.jpg', 'rb') as image:
+        yield {'file': image}
 
 # POST
-try:
-    # Can create RDF resources
+def test_create_ldp_rs(nodes):
+    # Can create an LDP-RS with POST
     r = requests.post(ROOT)
-    assert r.status_code == 201
-    NODES.add(r.headers['Location'])
-except AssertionError:
-    print('3.3 POST: Error creating an RDF resource')
+    assert r.status_code == 201, '3.3 POST: Error creating an LDP-RS'
+    nodes.add(r.headers['Location'])
 
-try:
-    # Creating an RDF resource returns constraints
-    r = requests.post(ROOT)
-    assert r.status_code == 201
-    assert 'rel="http://www.w3.org/ns/ldp#constrainedBy"' in r.headers['Link']
-    NODES.add(r.headers['Location'])
-except AssertionError:
-    print('3.3 POST: Constraints link missing when creating an RDF resource')
-
-try:
-    # Can create non-RDF resources
-    r = requests.post(ROOT, files=FILES)
-    assert r.status_code == 201
-    NODES.add(r.headers['Location'])
-except AssertionError:
-    print('3.3 POST: Error creating non-RDF resource')
-
-try:
-    # Creating a non-RDF resource returns constraints
-    r = requests.post(ROOT, files=FILES)
-    assert r.status_code == 201
-    NODES.add(r.headers['Location'])
-    assert 'rel="http://www.w3.org/ns/ldp#constrainedBy"' in r.headers['Link']
-except AssertionError:
-    print('3.3 POST: Constraints link missing when creating a non-RDF resource')
-
-try:
-    # Creating a non-RDF resource returns its RDF description resource
-    r = requests.post(ROOT, files=FILES)
-    assert r.status_code == 201
-    NODES.add(r.headers['Location'])
-    assert 'rel="describedby"' in r.headers['Link']
-except AssertionError:
-    print('3.3 POST: Link to RDF description resource missing from non-RDF resource creation response')
-
-try:
-    # Creating a non-RDF resource returns its RDF description resource
-    r = requests.post(ROOT, files=FILES)
-    assert r.status_code == 201
-    NODES.add(r.headers['Location'])
-    describes_nr = re.search('<(\S+)>; ?rel="describedby"', r.headers['Link'])
-    assert describes_nr is not None
+    # Defaults are advertised in the constraints
+    constraints_match = re.search('<(\S+)>; ?rel="http://www.w3.org/ns/ldp#constrainedBy"', r.headers['Link'])
+    assert constraints_match is not None, '3.3 POST: Constraints link missing when creating an LDP-RS'
     # group(1) is the captured group (\S+)
-    r = requests.get(describes_nr.group(1))
-    assert r.status_code == 200
+    constraints = requests.get(constraints_match.group(1))
+    assert 'interaction model' in constraints.text and 'default' in constraints.text, '3.3 POST: Default interaction model may be missing from constraints'
+
+def test_create_ldp_nr(nodes, files):
+    # Can create an LDP-NR with POST
+    r = requests.post(ROOT, files=files)
+    assert r.status_code == 201, '3.3 POST: Error creating an LDP-NR'
+    nodes.add(r.headers['Location'])
+
+    # Defaults are advertised in the constraints
+    constraints_match = re.search('<(\S+)>; ?rel="http://www.w3.org/ns/ldp#constrainedBy"', r.headers['Link'])
+    assert constraints_match is not None, '3.3 POST: Constraints link missing when creating an LDP-NR'
+    # group(1) is the captured group (\S+)
+    constraints = requests.get(constraints_match.group(1))
+    assert 'interaction model' in constraints.text and 'default' in constraints.text, '3.3 POST: Default interaction model may be missing from constraints'
+
+def test_describe_ldp_nr(nodes, files):
+    # Can create an LDP-NR with POST
+    r = requests.post(ROOT, files=files)
+    assert r.status_code == 201, '3.3 POST: Error creating an LDP-NR'
+    nodes.add(r.headers['Location'])
+
+    # Creating an LDP-NR returns the LDP-RS that describes it
+    describes_match = re.search('<(\S+)>; ?rel="describedby"', r.headers['Link'])
+    assert describes_match is not None, '3.3 POST: Link to LDP-RS describing new LDP-NR missing'
+    # group(1) is the captured group (\S+)
+    describes = requests.get(describes_match.group(1))
+    assert describes.status_code == 200, '3.3 POST: LDP-RS describing new LDP-NR was linked, but not created'
+    assert re.search('<'+r.headers['Location']+'>; ?rel="describes"', describes.headers['Link']) is not None, '3.5 GET: LDP-RS does not link to the LDP-NR it describes'
     # TODO: Assert it has type ldp#RDFSource
-except AssertionError:
-    print('3.3 POST: RDF description resource not created for non-RDF resource')
 
-try:
-    # Non-RDF resource with bad digest value returns 409
-    r = requests.post(ROOT, files=FILES, headers={'digest': 'md5=deadbeef'})
-    assert r.status_code == 409
-    #assert 'Checksum Mismatch' in r.text
-except AssertionError:
-    print('3.3.1 POST: Creating non-RDF resource with bad digest value works')
+def test_bad_digest(nodes, files):
+    # LDP-NR with bad digest value returns 409
+    r = requests.post(ROOT, files=files, headers={'digest': 'md5=deadbeef'})
+    assert r.status_code == 409, '3.3.1 POST: Creating LDP-NR with bad digest value should return 409'
+    if r.status_code == 201:
+        nodes.add(r.headers['Location'])
 
-try:
-    # Non-RDF resource with bad digest algorithm returns 400
+def test_bad_algo(nodes, files):
+    # LDP-NR with bad digest algorithm returns 400
     # TODO: How to query for accepted algorithms?
-    r = requests.post(ROOT, files=FILES, headers={'digest': 'md1=fakealgo'})
-    assert r.status_code == 400
-    #assert 'Unsupported Digest Algorithm' in r.text
-except AssertionError:
-    print('3.3.1 POST: Creating non-RDF resource with bad digest algorithm works')
-
-try:
-    # Create with remote content
-    pass
-except AssertionError:
-    print('POST: Remote content not supported')
+    r = requests.post(ROOT, files=files, headers={'digest': 'md1=fakealgo'})
+    assert r.status_code == 400, '3.3.1 POST: Creating LDP-NR with bad digest algorithm should return 400'
+    if r.status_code == 201:
+        nodes.add(r.headers['Location'])
 
 # GET
-# Set up
-r = requests.post(ROOT)
-r.raise_for_status
-rs = r.headers['Location']
-NODES.add(rs)
-r = requests.post(rs)
-r.raise_for_status
-rs_contained = r.headers['Location']
-NODES.add(rs_contained)
-r = requests.post(ROOT, files=FILES)
-r.raise_for_status
-nr = r.headers['Location']
-NODES.add(nr)
+def test_representation(nodes):
+    # Setup: Create LDP-RS
+    r = requests.post(ROOT)
+    r.raise_for_status
+    ldp_rs = r.headers['Location']
+    nodes.add(ldp_rs)
 
-try:
-    # A RDF resource links to the non-RDF resource it describes, if any
-    r = requests.get(nr)
-    describes_nr = re.search('<(\S+)>; ?rel="describedby"', r.headers['Link']).group(1)
-    r = requests.get(describes_nr)
-    assert r.status_code == 200
-    assert re.search('<'+nr+'>; ?rel="describes"', r.headers['Link']) is not None
-except AssertionError:
-    print('3.5 GET: RDF resource describing a non-RDF resource does not link to that non-RDF resource')
-
-try:
-    # RDF resource responds to the Prefer header
+    # LDP-RS responds to the Prefer header
     # TODO: How to query for Prefer header values?
     # TODO: Preference-Applied header always returned.
-    r = requests.get(rs, headers={'Prefer': 'return=representation'})
-    assert r.status_code == 200
-    assert r.headers['Preference-Applied'] == 'return=representation'
-    r = requests.get(rs, headers={'Prefer': 'return=minimal'})
-    assert r.status_code == 200
-    assert r.headers['Preference-Applied'] == 'return=minimal'
-except AssertionError:
-    print('3.5.2 GET: Preference-Applied header missing from response')
+    r = requests.get(ldp_rs, headers={'Prefer': 'return=representation'})
+    assert r.headers['Preference-Applied'] == 'return=representation', '3.5.2 GET: Preference-Applied header missing from response'
+    r = requests.get(ldp_rs, headers={'Prefer': 'return=minimal'})
+    assert r.headers['Preference-Applied'] == 'return=minimal', '3.5.2 GET: Preference-Applied header missing from response'
 
-try:
-    # RDF resource returns contained descriptions when asked
-    r = requests.get(rs, headers={'Prefer': 'return=representation; include="http://w3.org/ns/oa#PreferContainedDescriptions"'})
-    assert r.status_code == 200
-    assert re.search('ldp:contains\s*<'+rs_contained+'>', r.text) is not None
-    assert re.search('fedora:hasParent\s*<'+rs+'>', r.text) is not None
-except AssertionError:
-    print('3.5.1 GET: (MAY) Contained descriptions missing from response')
+def test_contained_desc(nodes):
+    # Setup: Create LDP-RSs
+    r = requests.post(ROOT)
+    r.raise_for_status
+    parent = r.headers['Location']
+    nodes.add(parent)
+    r = requests.post(parent)
+    r.raise_for_status
+    child = r.headers['Location']
+    nodes.add(child)
 
-try:
-    # RDF resource returns inbound references when asked
-    r = requests.get(rs, headers={'Prefer': 'return=representation; include="http://fedora.info/definitions/fcrepo#PreferInboundReferences"'})
+    # LDP-RS returns contained descriptions when asked
+    r = requests.get(parent, headers={'Prefer': 'return=representation; include="http://w3.org/ns/oa#PreferContainedDescriptions"'})
+    assert re.search('ldp:contains\s*<' + child +'>', r.text) is not None
+    assert re.search('fedora:hasParent\s*<' + parent + '>', r.text) is not None, '3.5.1 GET: (MAY) Contained descriptions missing from response'
+
+def test_inbound_refs(nodes):
+    # Setup: Create LDP-RSs
+    r = requests.post(ROOT)
+    r.raise_for_status
+    parent = r.headers['Location']
+    nodes.add(parent)
+    r = requests.post(parent)
+    r.raise_for_status
+    child = r.headers['Location']
+    nodes.add(child)
+
+    # LDP-RS returns inbound references when asked
+    r = requests.get(child, headers={'Prefer': 'return=representation; include="http://fedora.info/definitions/fcrepo#PreferInboundReferences"'})
     assert r.status_code == 200
     # TODO: how to test inbound refs?
-    raise AssertionError
-except AssertionError:
-    print('3.5.1 GET: (SHOULD) Inbound references missing from response')
+    assert 0, '3.5.1 GET: (SHOULD) Inbound references missing from response'
 
-try:
-    # Non-RDF resource responds to the Want-Digest header.
-    r = requests.get(nr, headers={'Want-Digest': 'md5'})
+def test_want_digest_header(nodes, files):
+    # Setup: Create LDP-NR
+    r = requests.post(ROOT, files=files)
+    r.raise_for_status
+    ldp_nr = r.headers['Location']
+    nodes.add(ldp_nr)
+
+    # LDP-NR responds to the Want-Digest header.
+    r = requests.get(ldp_nr, headers={'Want-Digest': 'md5'})
     assert r.status_code == 200
-    assert r.headers['Digest'].startswith('md5=')
-    r = requests.get(nr, headers={'Want-Digest': 'sha1'})
+    assert r.headers['Digest'].startswith('md5='), '3.5.3 GET: Want-Digest header ignored or incorrect response returned'
+    r = requests.get(ldp_nr, headers={'Want-Digest': 'sha1'})
     assert r.status_code == 200
-    assert r.headers['Digest'].startswith('sha1=')
-except AssertionError:
-    print('3.5.3 GET: Want-Digest header ignored or incorrect response returned')
+    assert r.headers['Digest'].startswith('sha1='), '3.5.3 GET: Want-Digest header ignored or incorrect response returned'
 
 # HEAD
-try:
+def test_empty_ldp_rs(nodes):
+    # Setup
+    r = requests.post(ROOT)
+    r.raise_for_status
+    ldp_rs = r.headers['Location']
+    nodes.add(ldp_rs)
+
     # Head request has no body
-    r = requests.head(ROOT)
-    assert r.status_code == 200
-    assert r.text == ''
-    r = requests.head(nr, headers={'Want-Digest': 'sha1'})
-    assert r.status_code == 200
-    assert r.headers['Digest'].startswith('sha1=')
-    assert r.text == ''
-except AssertionError:
-    print('3.6 HEAD: Unexpected response body for HEAD request')
+    head = requests.head(ldp_rs)
+    assert head.status_code == 200
+    assert head.text == '', '3.6 HEAD: Unexpected response body for HEAD request'
+
+def test_no_payload_headers(nodes):
+    # Setup
+    r = requests.post(ROOT)
+    r.raise_for_status
+    ldp_rs = r.headers['Location']
+    nodes.add(ldp_rs)
+
+    # Head request has no body
+    head = requests.head(ldp_rs)
+    for payload_header in PAYLOAD_HEADERS:
+        assert payload_header not in head.headers.keys(), '3.6 HEAD: (MAY) Payload header "' + payload_header + '" not ommitted'
+
+def test_same_headers(nodes):
+    # Setup
+    r = requests.post(ROOT)
+    r.raise_for_status
+    ldp_rs = r.headers['Location']
+    nodes.add(ldp_rs)
+
+    head = requests.head(ldp_rs)
+    get = requests.get(ldp_rs)
+    # Collect the headers from the HEAD request, without the payload headers.
+    head_keys = {key for key in head.headers.keys()}.difference(PAYLOAD_HEADERS)
+    # Collect the headers from the GET request, without the payload headers.
+    get_keys = {key for key in get.headers.keys()}.difference(PAYLOAD_HEADERS)
+    assert head_keys == get_keys, '3.6 HEAD: (SHOULD) Headers for a HEAD request should match the headers for a GET request'
+
+def test_empty_ldp_nr(nodes, files):
+    # Setup
+    r = requests.post(ROOT, files=files)
+    r.raise_for_status
+    ldp_nr = r.headers['Location']
+    nodes.add(ldp_nr)
+
+    head = requests.head(ldp_nr)
+    assert head.status_code == 200
+    assert head.text == '', '3.6 HEAD: Unexpected response body for HEAD request'
+
+def test_head_digest(nodes, files):
+    # Setup
+    r = requests.post(ROOT, files=files)
+    r.raise_for_status
+    ldp_nr = r.headers['Location']
+    nodes.add(ldp_nr)
+
+    # First without Want-Digest
+    head = requests.head(ldp_nr)
+    get = requests.get(ldp_nr)
+    assert ('Digest' in head.headers.keys()) == ('Digest' in get.headers.keys()), '3.6 HEAD: Presence of Digest header must be the same in HEAD and GET requests'
+
+    # Now with Want-Digest
+    head = requests.head(ldp_nr, headers={'Want-Digest': 'md5'})
+    get = requests.get(ldp_nr, headers={'Want-Digest': 'md5'})
+    assert ('Digest' in head.headers.keys()) == ('Digest' in get.headers.keys()), '3.6 HEAD: Presence of Digest header must be the same in HEAD and GET requests'
 
 # PUT
 
 # PATCH
 
 # DELETE
-# Set up
-DELETE_SUPPORTED = True
-r = requests.post(ROOT)
-r.raise_for_status
-rs = r.headers['Location']
-NODES.add(rs)
-r = requests.post(rs)
-r.raise_for_status
-child = r.headers['Location']
-NODES.add(child)
-r = requests.post(child)
-r.raise_for_status
-grandchild = r.headers['Location']
-NODES.add(grandchild)
+@support_delete
+def test_depth_zero(nodes):
+    # Set up
+    r = requests.post(ROOT)
+    r.raise_for_status
+    parent = r.headers['Location']
+    nodes.add(parent)
+    r = requests.post(parent)
+    r.raise_for_status
+    child = r.headers['Location']
+    nodes.add(child)
 
-try:
-    # TODO: How to query for DELETE support?
-    pass
-except AssertionError:
-    print('3.7 DELETE: (OPTIONAL) Delete method not supported')
-    DELETE_SUPPORTED = False
+    # Support deletion with depth of zero
+    # TODO: Depth of zero only supported for LDP-NRs?
+    # TODO: How to query for supported or default Depth values?
+    r = requests.delete(parent, headers={'Depth': '0'})
+    assert r.status_code == 204, '3.7.1 DELETE: Depth: 0 not supported'
+    r = requests.get(parent)
+    assert r.status_code == 410
+    r = requests.get(child)
+    assert r.status_code == 200, '3.7.1 DELETE: Depth:0 should delete the resource only'
 
-if DELETE_SUPPORTED:
-    try:
-        # Support deletion with depth of zero
-        # TODO: Depth of zero only supported for non-RDF resources?
-        # TODO: How to query for supported or default Depth values?
-        r = requests.delete(rs, headers={'Depth': '0'})
-        assert r.status_code == 204
-        r = requests.get(rs)
-        assert r.status_code == 410
-        r = requests.get(child)
-        assert r.status_code == 200
-    except AssertionError:
-        print('3.7.1 DELETE: Depth: 0 not supported')
+@support_delete
+def test_depth_infinity(nodes):
+    # Set up
+    r = requests.post(ROOT)
+    r.raise_for_status
+    parent = r.headers['Location']
+    nodes.add(parent)
+    r = requests.post(parent)
+    r.raise_for_status
+    child = r.headers['Location']
+    nodes.add(child)
+    r = requests.post(child)
+    r.raise_for_status
+    grandchild = r.headers['Location']
+    nodes.add(grandchild)
 
-    try:
-        # Support deletion with depth of infinity
-        r = requests.delete(child, headers={'Depth': 'infinity'})
-        assert r.status_code == 204
-        r = requests.get(child)
-        assert r.status_code == 410
-        r = requests.get(grandchild)
-        assert r.status_code == 410
-    except AssertionError:
-        print('3.7.1 DELETE: Depth: infinity not supported')
-
-# Clean up
-for node in NODES:
-    r = requests.delete(node)
-    r = requests.delete(node+'/fcr:tombstone')
+    # Support deletion with depth of infinity
+    r = requests.delete(parent, headers={'Depth': 'infinity'})
+    assert r.status_code == 204, '3.7.1 DELETE: Depth: infinity not supported'
+    r = requests.get(parent)
+    assert r.status_code == 410
+    r = requests.get(grandchild)
+    assert r.status_code == 410
